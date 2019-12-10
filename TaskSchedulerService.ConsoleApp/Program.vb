da493367d077677462e4c1b960fdca20db574e3c
Mariano Villa <Mariano.Villa@outlook.com.ar>
2019-12-09 22:19:31 -0300

    Added logging functionality.

--- TaskSchedulerService.ConsoleApp/Program.vb
+++ TaskSchedulerService.ConsoleApp/Program.vb
@@ -1,17 +1,50 @@
 ﻿Imports System.Configuration
+Imports Alpha.Utilidades.General
 Imports TaskSchedulerService.Lib
 
 Module Program
 
+    ''' <summary>
+    ''' The main program. For more details on the implementations, <see cref="Examples"/> and the tests.
+    ''' </summary>
     Sub Main()
 
-        'Run processes:
-        Dim Executables = ConfigurationManager.AppSettings("ExecutablesToRun").Split("|")
-        Dim Proc = TasksHandler.RunProcess(Executables)
+        ConfigureLog()
+        Try
 
-        'Mail:
-        Dim Mailing = New MailHelper("TaskSchedulerService")
-        Mailing.Send("I've executed the notepad.")
+            'A general approach would be to run processes from the AppSettings:
+            Dim Executables = ConfigurationManager.AppSettings("ExecutablesToRun").Split("|")
+            Dim Proc = TasksHandler.RunProcess(Executables)
+
+            'Mail:
+            Dim Mailing = New MailHelper("TaskSchedulerService")
+            Mailing.Send("I've executed the notepad.")
+
+        Catch ex As Exception
+
+            'Log an eventual exception:
+            LogsUtils.Loguear("Something went wrong: " + ex.Message + "\n" + ex.StackTrace)
+
+        End Try
+
+    End Sub
+
+    Sub ConfigureLog()
+        Dim ConfiguracionLog = New ConfiguracionLog
+
+        With ConfiguracionLog
+            .LoguearWebApi = False
+            .NombreArchivoLog = "TaskSchedulerService"
+            .ProyectoOrigen = "TaskSchedulerService.ConsoleApp"
+        End With
+
+        LogsUtils.Configuracion = ConfiguracionLog
+
+        If (ConfigurationManager.AppSettings("Debug") = "1") Then
+            LogsUtils.Configuracion.Debug = True
+        ElseIf (ConfigurationManager.AppSettings("Debug") = "0") Then
+            LogsUtils.Configuracion.Debug = False
+        End If
 
     End Sub
 
